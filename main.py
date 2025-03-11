511e9db8d951f4826ce4ec0c19461cccb004749d
Giovanni <ivanalipov4@gmail.com>
2025-03-11 09:08:59 +0100

    newcommit

---
+++ main.py
@@ -0,0 +1,29 @@
+# name = input("Enter your name: ")
+# surname = input("Enter your surname: ")
+# age = int(input("Enter your age: "))
+# growth = ""
+
+# if age <= 10:
+#     growth = "child"
+# elif age > 10 and age <= 18:
+#     growth = "teenager"
+# elif age > 18 and age <= 50:
+#     growth = "adult"
+# elif age > 50:
+#     growth = "old"
+# else:
+#     growth = "unknown"
+
+
+# if name == "Giovanni" and surname == "Rossi":
+#     print("Welcome back, my Lord!")
+# else:
+#     print(f"Hello, {name} {surname}! You are a {growth}.")
+
+
+# Made with love by Giovanni Rossi
+# ```
+
+
+
+
